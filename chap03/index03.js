818efba998f54e534a43960220f915b88044f0a8
BackHoe312 <61866551+BackHoe312@users.noreply.github.com>
2021-09-24 11:04:26 +0900

    Update index03.js

--- chap03/index03.js
+++ chap03/index03.js
@@ -5,10 +5,14 @@ const { token } = require(__dirname + '/../config.json');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.commands = new Collection();
-//
+const commandFiles = fs.readdirSync(__dirname + '/../commands')
+    .filter(file => file.endsWith('.js'));
+// fs.readdirSync() => 해당 디렉토리에 있는 모든 파일 이름의 배열을 반환
+// endWith() => 매개변수로 입력한 문자열로 끝나는 파일이 아닌 것은 걸러서
 
 for (const file of commandFiles) {
-    //
+    const command = require(__dirname + `/../commands/${file}`);
+    client.commands.set(command.data.name, command);
 }
 
 client.once('ready', () => {
@@ -18,16 +22,18 @@ client.once('ready', () => {
 client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return;
 
-    //
+    const command = client.commands.get(interaction.commandName);
+    // 클라이언트에 들어간 command 중 사용자가 입력한 command가 있으면 해당 파일을 json를 반환
 
     if (!command) return;
 
     try {
-        //
+        await command.execute(interaction);
     } catch (err) {
         console.error(err);
         await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        // ephemeral => 입력한 사용자만 볼 수 있음.
     }
 });
 
-client.login(token);
+client.login(token); 
